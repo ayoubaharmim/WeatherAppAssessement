159c3d43b86391bb1b3e723fd6078179c7ef2373
Ayoub Aharmim <33521285+ayoubaharmim@users.noreply.github.com>
2023-04-23 15:30:31 +0100

    feat: create list item component

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,5 +1,12 @@
-import React, { useCallback, useState } from "react";
-import { StyleSheet, Text, TextInput, View, Button, FlatList } from "react-native";
+import React, {useCallback, useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  FlatList, TouchableOpacity,
+} from "react-native";
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {getCities, setCity, useCityState} from '../core';
 
@@ -10,8 +17,6 @@ export const Main = () => {
   const [cities, setCities] = useState(getCities());
   const [newCity, setNewCity] = useState('');
 
-  console.warn(cities);
-
   const addCity = useCallback(() => {
     setCity(newCity);
     setNewCity('');
@@ -19,7 +24,7 @@ export const Main = () => {
   }, [newCity]);
 
   const renderCityItem = ({item}: {item: string}) => {
-    return <Text>{item}</Text>;
+    return <CityListItem city={item} onDelete={() => {}} navigate={() => {}} />;
   };
 
   return (
@@ -29,6 +34,7 @@ export const Main = () => {
         paddingBottom: insets.bottom,
         paddingLeft: insets.left,
         paddingRight: insets.right,
+        ...styles.container,
       }}>
       <View style={styles.inputContainer}>
         <TextInput
@@ -42,16 +48,52 @@ export const Main = () => {
         <Button title="Add" onPress={addCity} disabled={newCity.length === 0} />
       </View>
 
-      <FlatList data={cities} renderItem={renderCityItem} />
+      {cities.length === 0 ? (
+        <Text>Enter A City</Text>
+      ) : (
+        <FlatList data={cities} renderItem={renderCityItem} />
+      )}
     </View>
   );
 };
 
+const CityListItem = React.memo(
+  ({
+    city,
+    onDelete,
+    navigate,
+  }: {
+    city: string;
+    onDelete: () => void;
+    navigate: () => void;
+  }) => {
+    return (
+      <View style={styles.cityItem}>
+        <Text style={styles.cityText}>{city}</Text>
+
+        <TouchableOpacity onPress={navigate}>
+          <Text style={{color: 'green', fontSize: 14}}>View Weather</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onDelete}>
+          <Text style={{color: 'red', fontSize: 14, fontWeight: 'bold'}}>
+            Delete
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  },
+);
+
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F1F6F9',
+  },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     paddingHorizontal: 16,
+    marginBottom: 12,
   },
   input: {
     flex: 1,
@@ -60,5 +102,21 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 8,
     marginRight: 8,
+  },
+  cityItem: {
+    height: 60,
+    flexDirection: 'row',
+    backgroundColor: '#9BA4B5',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    marginVertical: 4,
+    marginHorizontal: 12,
+    alignItems: 'center',
+    borderRadius: 20,
+  },
+  cityText: {
+    fontSize: 18,
+    fontWeight: 'bold',
   },
 });
