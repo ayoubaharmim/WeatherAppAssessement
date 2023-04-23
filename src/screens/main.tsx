5d574b8b8be2e7b0b9be2ae3b6f961ab3a98a3f8
Ayoub Aharmim <33521285+ayoubaharmim@users.noreply.github.com>
2023-04-23 14:52:52 +0100

    feat: add city input

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, TextInput, View, Button} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import { useCityState } from "../core";
+import {getCities, useCityState} from '../core';
 
 export const Main = () => {
   const insets = useSafeAreaInsets();
   const {cityState} = useCityState();
-  console.warn(cityState);
+
+  const [cities, setCities] = useState(getCities());
+  const [newCity, setNewCity] = useState('');
+
+  console.warn(cities);
   return (
     <View
       style={{
@@ -15,7 +19,26 @@ export const Main = () => {
         paddingLeft: insets.left,
         paddingRight: insets.right,
       }}>
-      <Text>Main Screen</Text>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="Enter a city..." />
+        <Button title="Add" onPress={() => {}} disabled={newCity.length === 0} />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    padding: 8,
+    marginRight: 8,
+  },
+});
