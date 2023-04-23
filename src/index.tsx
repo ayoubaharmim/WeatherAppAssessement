1c1998eac9be890afcc5f39691262fde5473a1a0
Ayoub Aharmim <33521285+ayoubaharmim@users.noreply.github.com>
2023-04-23 16:10:54 +0100

    feat: integrate query provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,20 @@ import {RootNavigator} from './navigation';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {cityReducer, defaultCityState, CityContext} from './core';
+import {QueryClient, QueryClientProvider} from 'react-query';
 
 export const App = () => {
   const [cityState, setCityState] = useReducer(cityReducer, defaultCityState);
+  const queryClient = new QueryClient();
   return (
-    <CityContext.Provider value={{cityState, setCityState}}>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <RootNavigator />
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </CityContext.Provider>
+    <QueryClientProvider client={queryClient}>
+      <CityContext.Provider value={{cityState, setCityState}}>
+        <SafeAreaProvider>
+          <NavigationContainer>
+            <RootNavigator />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </CityContext.Provider>
+    </QueryClientProvider>
   );
 };
